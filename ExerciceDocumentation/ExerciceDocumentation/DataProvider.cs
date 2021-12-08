8e259c49484917e215189a90bfbeb798bb856084
Keven Lambert <2065551@cegepba.qc.ca>
2021-12-08 08:49:41 -0500

    Exercice

---
+++ ExerciceDocumentation/ExerciceDocumentation/DataProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Cette classe est utilisée dans le contexte d'un exercice pour se pratiquer à créer de la documentation.
+/// 
+/// Le DataProvider permet d'accéder à des données à partir d'une base de données de type SQLServer.
+/// Pour l'initialiser, vous devez avoir en main votre connection string. Vous pouvez initialiser la classe de cette manière:
+/// <code>
+///     DataProvider data = new DataProvider("connexionAVotreDb");
+/// </code>
+/// 
+/// Vous pouvez accéder à toute les données avec une liste de string:
+/// <code>
+///     foreach (var data in data.GetAllData()
+///     {
+///         lstDisplay.Items.Add(data);
+///     }
+/// </code>
+/// 
+/// ou recevoir une ligne de données directement avec son index.:
+/// <code>
+///     lstDisplay.Items.Add(data.GetSingleLineOfData(3)
+///     <param name="indexOfData">L'index de la ligne désirée dans la base de données.</param>
+/// </code>
+/// 
+/// </summary>
+namespace ExerciceDocumentation
+{
+    public class DataProvider
+    {
+        public List<string> DataCollection { get; }
+
+        public SqlConnection Connection { get; }
+
+        public DataProvider(string connectionString)
+        {
+            Connection.ConnectionString = connectionString;
+        }
+
+        public List<string> GetAllData()
+        {
+            return DataCollection;
+        }
+
+        public string GetSingleLineOfData(int indexOfData)
+        {
+            return DataCollection[indexOfData];
+        }
+    }
+}
